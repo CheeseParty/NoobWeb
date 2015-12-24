5a4d971de47fb70347b8e47ff09078833a85ffa5
Gweedzy <pj.leborgne@gmail.com>
2015-12-24 16:37:29 +0100

    design 2

---
+++ index.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Nous créons votre site Web</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <div id="topbar">
+            <img src="logo.svg" alt="logo" class="logo">
+            <ul id="navbar">
+                <li class="currnavlink">Accueil</li>
+                <li class="navlink">A propos de nous</li>
+                <li class="navlink">Nos créations</li>
+                <li class="navlink">Faire une commande</li>
+                <li class="navlink">Nous contacter</li>
+            </ul>
+            
+            <div id="reglog">
+                <a href="#" class="regbtn">S'inscrire</a>
+                <a href="#" class="logbtn">Se connecter</a>
+            </div>
+        </div>
+        
+        <div id="firstblock">
+            
+        </div>
+    </body>
+</html>
