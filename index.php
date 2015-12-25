9f0279e985b4ead22ccf6e3dd12531e4be10760b
Gweedzy <pj.leborgne@gmail.com>
2015-12-25 01:53:03 +0100

    Menubar, 1er bloc & 2eme bloc
    Barre de menu avec les différentes pages, s'inscrire & se connecter.
    Container avec un formulaire d'inscription. Container avec une
    description (qui contient les svg, résumé, blablabla)

--- index.php
+++ index.php
@@ -4,27 +4,87 @@
         <title>Nous créons votre site Web</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="style.css">
+        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
     </head>
     
     <body>
         <div id="topbar">
-            <img src="logo.svg" alt="logo" class="logo">
-            <ul id="navbar">
-                <li class="currnavlink">Accueil</li>
-                <li class="navlink">A propos de nous</li>
-                <li class="navlink">Nos créations</li>
-                <li class="navlink">Faire une commande</li>
-                <li class="navlink">Nous contacter</li>
-            </ul>
-            
-            <div id="reglog">
-                <a href="#" class="regbtn">S'inscrire</a>
-                <a href="#" class="logbtn">Se connecter</a>
+            <div id="topbarcontent">
+                <a class="sitename" href="index.php">NoobWeb</a>
+                
+                <ul id="navbar">    
+                    <li><a href="#" class="navlink">A propos</a></li>
+                    <li><a href="#" class="navlink">Nos créations</a></li>
+                    <li><a href="#" class="navlink">Faire une commande</a></li>
+                    <li><a href="#" class="navlink">Nous contacter</a></li>
+                </ul>
+
+                <div id="reglog">
+                    <a href="#" class="regbtn">S'inscrire</a>
+                    <a href="#" class="logbtn">Se connecter</a>
+                </div>
+                
             </div>
         </div>
         
-        <div id="firstblock">
+        <div id="firstblockimg">
+            <div id="firstblockimgcontent">
+                <div id="textzone">
+                    <p class="title">Inscrivez vous et commandez votre site gratuitement !</p>
+                    <p class="about">Où est le piège ? Il n'y en a pas ! Les premières commandes seront effectuées gratuitement !</p>
+                </div>
+                
+                <div id="regzone">
+                    <!-- Formulaire provisoire hein, on ajoutera des champs je pense -->
+                    <form method="post" action="#">
+                        <input type="text" class="regforminput" name="identity" placeholder="Prénom NOM">
+                        <input type="email" class="regforminput" name="email" placeholder="adressemail@example.com">
+                        <input type="password" class="regforminput" name="password" placeholder="Mot de passe">
+                        <input type="password" class="regforminput" name="password2" placeholder="Confirmer le mot de passe">
+                        <input type="submit" class="regformbtn" value="S'inscrire">
+                    </form>
+                    <!--<p class="cu-confirmtext">En vous inscrivant, vous certifiez avoir les Conditions d'utilisation de notre site et les acceptez.</p>-->
+                </div>
+            </div>
+        </div>
+        
+        <div id="secondblockabout">
+            <div class="box">
+                <img class="boximg" src="speed2.svg" alt="speedimgnoobweb">
+                <p class="boxtitle">Rapidité</p>
+                <p class="boxtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum arcu ut elit tincidunt, ut feugiat justo lacinia. In hac habitasse platea dictumst.                  Praesent maximus dui lectus, non fringilla eros dapibus vitae. Etiam non arcu ipsum. Nam venenatis, est tincidunt lobortis tincidunt, felis nisi elementum lectus, ac                           elementum neque quam consectetur quam. Morbi eget lectus pulvinar, porttitor arcu efficitur, pretium ante. </p>
+            </div>
             
+            <div class="box">
+                <img class="boximg" src="quality.svg" alt="qualityimgnoobweb">
+                <p class="boxtitle">Qualité</p>
+                <p class="boxtext">Maecenas fringilla, augue at bibendum dapibus, metus magna sodales dui, nec vestibulum libero magna vitae dolor. Duis in neque ac lacus euismod luctus.                       Fusce nunc massa, auctor et velit quis, sodales commodo nulla. Praesent tincidunt euismod bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur                     ridiculus mus. Vestibulum ut nulla non metus consectetur egestas nec eu tellus.</p>
+            </div>
+            
+            <div class="box">
+                <img class="boximg" src="smartphone.svg" alt="mobileimgnoobweb">
+                <p class="boxtitle">Compatibilité</p>
+                <p class="boxtext">Maecenas fringilla, augue at bibendum dapibus, metus magna sodales dui, nec vestibulum libero magna vitae dolor. Duis in neque ac lacus euismod luctus.                       Fusce nunc massa, auctor et velit quis, sodales commodo nulla. Praesent tincidunt euismod bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur                     ridiculus mus. Vestibulum ut nulla non metus consectetur egestas nec eu tellus.</p>
+            </div>
+            
+            <div class="box">
+                <img class="boximg" src="fiabilité.svg" alt="fiabilitéimgnoobweb">
+                <p class="boxtitle">Fiabilité</p>
+                <p class="boxtext">Maecenas fringilla, augue at bibendum dapibus, metus magna sodales dui, nec vestibulum libero magna vitae dolor. Duis in neque ac lacus euismod luctus.                       Fusce nunc massa, auctor et velit quis, sodales commodo nulla. Praesent tincidunt euismod bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur                     ridiculus mus. Vestibulum ut nulla non metus consectetur egestas nec eu tellus.</p>
+            </div>
+            
+            <div class="box">
+                <img class="boximg" src="originalité.svg" alt="originalitéimgnoobweb">
+                <p class="boxtitle">Originalité</p>
+                <p class="boxtext">Maecenas fringilla, augue at bibendum dapibus, metus magna sodales dui, nec vestibulum libero magna vitae dolor. Duis in neque ac lacus euismod luctus.                       Fusce nunc massa, auctor et velit quis, sodales commodo nulla. Praesent tincidunt euismod bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur                     ridiculus mus. Vestibulum ut nulla non metus consectetur egestas nec eu tellus.</p>
+            </div>
+            
+             <div class="box">
+                <img class="boximg" src="price.svg" alt="priximgnoobweb">
+                <p class="boxtitle">Petit prix</p>
+                <p class="boxtext">Maecenas fringilla, augue at bibendum dapibus, metus magna sodales dui, nec vestibulum libero magna vitae dolor. Duis in neque ac lacus euismod luctus.                       Fusce nunc massa, auctor et velit quis, sodales commodo nulla. Praesent tincidunt euismod bibendum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur                     ridiculus mus. Vestibulum ut nulla non metus consectetur egestas nec eu tellus.</p>
+            </div>
         </div>
     </body>
 </html>
