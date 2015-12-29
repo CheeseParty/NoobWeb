b721e796f9da35b86383d9749a820d05fa036a6e
JoJoMimosa <joachimrochat@gmail.com>
2015-12-29 22:13:54 +0100

    Hello World

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* MAIN.JS
+ * Script principal
+ */
+
+// Menu pour les petits appareils
+$(".menu-btn").click(function() {
+    $(".header-container").css("height","100vh");
+    $(".header-nav").css("display","block");
+});
