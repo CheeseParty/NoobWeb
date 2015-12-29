a2c77ad54316028d048334fa87880422c63983ee
JoJoMimosa <joachimrochat@gmail.com>
2015-12-29 23:49:26 +0100

    hhh

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 /* MAIN.JS
  * Script principal
  */
-
-// Menu pour les petits appareils
-$(".menu-btn").click(function() {
-    $(".header-container").css("height","100vh");
-    $(".header-nav").css("display","block");
-});
