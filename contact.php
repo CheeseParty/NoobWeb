f9df3a489e2e79aec482e0f16418f416b518fa7b
Gweedzy <pj.leborgne@gmail.com>
2015-12-29 23:16:03 +0100

    Formulaire de contact
    Création du formulaire de contact (aspect seulement) à intégrer sur la
    page du site.

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>NoobWeb | Contact</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,500" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/contact.css">
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body>
+        <div id="contactsection">
+            <form method="post">
+                <h1>Contactez nous</h1>
+                <input type="text" name="nom" class="textboxform" placeholder="Prénom NOM">
+                <input type="email" name="email" class="textboxform" placeholder="Adresse mail (example@domaine.fr)">
+                <input type="text" name="entreprise" class="textboxform" placeholder="Nom de votre entreprise">
+                <textarea name="details" rows="8" class="textboxform" placeholder="Détails de votre projet, informations sur votre entreprise.. Notez que certaines informations pourront être facteur du design de votre site, selon la clientèle visée, le statut de l'entreprise.."></textarea>
+                <input type="submit" value="Envoyer le message" class="contactformbtn">
+            </form>
+        </div>
+    </body>
+</html>
