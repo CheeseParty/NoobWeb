7e15ed3428ca6e2ae818cd72c28b0c1c69802400
Gweedzy <pj.leborgne@gmail.com>
2015-12-30 15:24:02 +0100

    Edit Contact Form & ajout description.php
    Edit du formulaire de contact et ajout de la page description.php -
    contenu à copier sur l'accueil -

--- description.php
+++ description.php
@@ -1,16 +1,16 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html>
-    <head>
+   <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>NoobWeb | Contact</title>
+        <title>NoobWeb | Description</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
         <link rel="stylesheet" href="css/normalize.min.css">
         <link href="https://fonts.googleapis.com/css?family=Poppins:300,500" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="css/contact.css">
+        <link rel="stylesheet" href="css/description.css">
 
         <!--[if lt IE 9]>
             <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
@@ -18,17 +18,11 @@
     </head>
     
     <body>
-
-        <section class="contact-section">
-            <form method="post">
-                <h2>Contactez-nous</h2>
-                <input type="text" class="textbox-form" placeholder="Prénom NOM">
-                <input type="email" class="textbox-form" placeholder="Adresse E-mail">
-                <input type="text" class="textbox-form" placeholder="Nom de votre entreprise">
-                <textarea rows="8" class="textbox-form" placeholder="Détails de votre projet, informations sur votre entreprise.. "></textarea>
-                <input type="submit" value="Envoyer le message" class="contact-form-btn">
-            </form>
-        </section>
+        <div id="descriptionsection">
+            
+        
+            
+        </div>
         
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
