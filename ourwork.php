cdbb32b44f0836616d08508880f71e3a4525391f
Gweedzy <pj.leborgne@gmail.com>
2015-12-30 17:48:15 +0100

    Creation Notre travail + SVG Ecrans
    Creation du bloc de "Notre Travail" :+1:

---
+++ ourwork.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>NoobWeb | Notre Travail</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,500" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/ourwork.css">
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body>
+        <section class="our-work">
+            <img src="compatibility1.svg" alt="Ordinateur, Mobile & Tablette, Compatibilité de votre site" class="our-work-big-img">
+            <span class="our-work-textzone">
+                <h2>Notre Travail</h2>
+                <p class="our-work-text">
+                    Sed in mi feugiat, scelerisque turpis quis, varius lacus. Phasellus iaculis, velit id vehicula aliquet, neque libero laoreet orci, nec molestie nibh lorem a velit. Nam                         posuere nisi vitae libero vulputate gravida. Nulla vitae consequat odio, ut pretium orci. Sed efficitur euismod hendrerit. Phasellus facilisis egestas enim in ullamcorper.                     Nulla non blandit tellus, eu hendrerit ligula. Proin porta posuere augue tempus sagittis. Vivamus nisi odio, elementum vel convallis in, imperdiet id dolor. Mauris risus                       leo, porta eu viverra eget, euismod id nulla. Vivamus ultrices augue mattis metus consectetur, id posuere leo placerat. Duis sit amet orci commodo, sagittis odio non,                         porta tortor. Proin sit amet accumsan justo, nec cursus odio. Integer semper turpis dui, ac semper dolor rutrum vel.
+                </p>
+            </span>   
+        </section>
+    </body>
+</html>
