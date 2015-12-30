56704568bf9c6c78b7abc59e8f0abb486b8f8c56
JoJoMimosa <joachimrochat@gmail.com>
2015-12-31 00:55:48 +0100

    Le menu du jour
    Je suis fatigué
    Désolé Paul

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 /* MAIN.JS
  * Script principal
  */
+
+// Toggling du menu
+$(".menu-btn").click(function() {
+    // Variables
+    var nav = $("header nav");
+    
+    // Toggling
+    if ( nav.css("display") == "none" ) {
+        nav.css("display","block");
+        setTimeout(function() {
+            nav.css("opacity",1);
+            nav.css("width","100%");
+        },0);
+    } else {
+        nav.css("opacity","0");
+        nav.css("width","110%");
+        setTimeout(function() {
+            nav.css("display","none");
+        },200);
+    }
+    console.log("done");
+});
+
