1f72c296f06998b403738784c6f0ef9df226dc04
Gweedzy <pj.leborgne@gmail.com>
2015-12-31 15:19:02 +0100

    teqsqsdf

---
+++ welcome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>NoobWeb | Bienvenue</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,500" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/welcome.css">
+        <link rel="stylesheet" href="css/main.css">
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body>
+        <section class="welcome">
+            <p class="welcome-text">Réalisation des premiers sites gratuitement* ! Commandez le votre dès maintenant et bénéficiez de cette offre exceptionnelle <a href="#" class="go-btn">Faire sa                      commande</a></p>
+        </section>
+    
+    </body>
