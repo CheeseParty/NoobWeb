2bbdb4ae742f8f393f41f1c0dfa1142e7d39b8f2
JoJoMimosa <joachimrochat@gmail.com>
2016-01-03 17:58:54 +0100

    Ajout: Notre travail

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@
  * Script principal
  */
 
-// Listener du bouton menu
-$('.menu-btn').on("click",toggleMenu);
+// FONCTIONS
 
 // Fonction de toggling du menu
 function toggleMenu() {
@@ -20,7 +19,7 @@ function toggleMenu() {
     if ( nav.hasClass('menu-hidden') ) {
         menu_btn.css('background','#EAEAEA');
         // Listener de fermeture sur le main
-        main.off("click",toggleMenu);
+        main.off('click',toggleMenu);
         setTimeout(function() {
             nav.css('display','none');
         },200);
@@ -28,7 +27,50 @@ function toggleMenu() {
         menu_btn.css('background','#d1d1d1');
         nav.css('display','block');
         // Listener de fermeture sur le main
-        main.on("click",toggleMenu);
+        main.on('click',toggleMenu);
     }
 };
 
+// Fonction de debounce
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Executé au resize et au chargement (debounced)
+var deBouncedResize = debounce(function() {
+    if ( $('body').width() > 768 ) {
+        // Smooth Scroll aux ancres
+        $('a').smoothScroll({offset: -100});
+        // Remise en place du nav
+        $('header nav')
+            .removeClass('menu-shown')
+            .addClass('menu-hidden')
+            .removeAttr('style');
+        $('.menu-btn').css('background','#EAEAEA');
+    } else {
+        // Smooth Scroll aux ancres
+        $('a').smoothScroll({offset: -60});
+        // Listener du bouton et des liens du menu
+        $('.menu-btn').off();
+        $('.header nav a').off();
+        $('.menu-btn').click(toggleMenu);
+        $('header nav a').click(toggleMenu);
+    }
+}, 250);
+
+// INITIALISATIONS
+
+// Divers listeners sensibles à la largeur d'écran (debounced)
+$(window).resize(deBouncedResize);
+deBouncedResize();
